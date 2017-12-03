b15e5199ee7486ae3b9e649542b19d86c8e0c518
Che Perth <chejesusperth@yahoo.com>
2017-12-02 23:11:31 -0800

    Create PrintingChoices.java
    This is the original code that was provided in the lesson. The lesson asks you to rearrange text, add text and print out extra spaces 
    with System.out.println(" "); 
    
    
    public class PrintingChoices
    {
     public static void main( String[]args)
     {
      System.out.println("Alpha");
      System.out.println("Bravo"); 
      
      System.out.println("Charlie"); 
      System.out.println("Delta"); 
      System.out.println(); 
      
      System.out.print("Echo"); 
      System.out.print("Foxtrot"); 
      
      System.out.println("Golf");  
      System.out.print("Hotel"); 
      System.out.println(); 
      System.out.println("India"); 
      
      System.out.println(); 
      System.out.println("This" + " " + "is" + " " + "a" + " test."); 
     }
    }

---
+++ PrintingChoices.java
@@ -0,0 +1,23 @@
+public class PrintingChoices
+{
+ public static void main( String[]args)
+ {
+  System.out.println("Alpha");
+  System.out.println("Bravo"); 
+  System.out.println("Charlie"); 
+  System.out.println("Delta"); 
+  System.out.print("Echo"); 
+  System.out.println(); 
+  System.out.print("Foxtrot"); 
+  System.out.println(); 
+  System.out.println("Golf");  
+  System.out.print("Hotel"); 
+  System.out.println(); 
+  System.out.println("India"); 
+  System.out.println(); 
+  System.out.println("This" + " " + "is" + " " + "test."); 
+  System.out.println(); 
+  System.out.print("I'm" + " " + "learning" + " " + "Java");  
+  System.out.println("the" + " " + "hard" + " " + "way."); 
+ }
+}
