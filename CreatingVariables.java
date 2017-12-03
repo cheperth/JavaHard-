40f510df3ff38c7ac624ccb988edee59c36784cb
Che Perth <chejesusperth@yahoo.com>
2017-12-02 23:28:30 -0800

    Create CreatingVariables.java
    Below is the original code that was provided. The goal was to add integer variables, double variables, String variables, change values of variables and print out the new variables with the new values. 
    public class CreatingVariables { 
    public static void main( String[] args ) { 
    int x, y, age; 
    double seconds, e, checking; 
    String firstName, lastName, title; 
     
     x = 10; 
     y = 400; 
     age = 39;
    
    seconds = 4.71; 
    e = 2.71828182845904523536; 
     checking = 1.89; 
     
    firstName = "Graham"; 
    lastName = "Mitchell";
    title = "Mr."; 
     
    System.out.println( "The variable x contains " + x ); 
    System.out.println( "The value " + y + " is stored in the variable y." ); 
    System.out.println( "The experiment took " + seconds + " seconds." ); 
    System.out.println( "A favorite irrational # is Euler's number: " + e ); 
    System.out.println( "Hopefully you have more than $" + checking + "!" ); 
    System.out.println( "My name's " + title + " " + firstName + lastName ); 
     } 
    }

---
+++ CreatingVariables.java
@@ -0,0 +1,34 @@
+public class CreatingVariables
+{
+ public static void main(String[]args)
+ {
+  int x, y, age, before; 
+  double seconds, e, checking, beauty; 
+  String firstName, lastName, title, Chest, wall; 
+  
+  x = 10; 
+  y = 400; 
+  age = 39; 
+  before = 69; 
+  
+  beauty = 200.12;
+  seconds = 4.71; 
+  e = 2.71828182845904523536;
+  checking = 1.89; 
+  
+  wall = "nuts"; 
+  Chest = "Nuts"; 
+  firstName = "Graham"; 
+  lastName = "Mitchell";
+  title = "Mr."; 
+  
+  System.out.println("The variable x contains " + x); 
+  System.out.println("The value " + y + " is stored in the variable y." ); 
+  System.out.println("The experiment took " + seconds + " seconds." ); 
+  System.out.println("A favorite irrational # is Euler's number:" + e ); 
+  System.out.println("Hopefully you have more than $" + checking + "!" ); 
+  System.out.println("My name's " + title + " " + firstName + lastName ); 
+  System.out.println(age + before + beauty + " " + wall + " " + Chest ); 
+ }
+}
+  
