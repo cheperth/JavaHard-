bf6bf87318fb6dc58ffd3d9e8df7185d2e06ab63
Che Perth <chejesusperth@yahoo.com>
2017-12-02 23:55:39 -0800

    Create MathOperations.java

---
+++ MathOperations.java
@@ -0,0 +1,34 @@
+ //This is the code provided for MathOperations before any changes were made 
+ 
+ public class MathOperations { 
+  public static void main( String[] args ) { 
+  int a, b, c, d, e, f, g; 
+  double x, y, z; 5 String one, two, both; 
+  
+  a = 10; 
+  b = 27; 
+  System.out.println( "a is " + a + ", b is " + b ); 
+  
+  c = a + b; 
+  System.out.println( "a+b is " + c ); 
+  d = a - b; 
+  System.out.println( "a-b is " + d ); 
+  e = a+b*3; 
+  System.out.println( "a+b*3 is " + e ); 
+  f = b / 2; 
+  System.out.println( "b/2 is " + f ); 
+  g = b % 10; 20 System.out.println( "b%10 is " + g ); 
+  
+  x = 1.1; 
+  System.out.println( "\nx is " + x ); 
+  y = x*x; 
+  System.out.println( "x*x is " + y ); 
+  z = b / 2; 
+  System.out.println( "b/2 is " + z ); 
+  System.out.println(); 
+  one = "dog"; 
+  two = "house"; 
+  both = one + two; 
+  System.out.println( both );
+ } 
+ }
