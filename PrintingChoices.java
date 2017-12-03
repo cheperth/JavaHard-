93bf83dfbd6a0373f16a92e2131ea31f3a2dd546
Che Perth <chejesusperth@yahoo.com>
2017-12-02 23:49:04 -0800

    Update PrintingChoices.java

--- PrintingChoices.java
+++ PrintingChoices.java
@@ -1,23 +1,19 @@
-public class PrintingChoices
-{
- public static void main( String[]args)
- {
-  System.out.println("Alpha");
-  System.out.println("Bravo"); 
-  System.out.println("Charlie"); 
-  System.out.println("Delta"); 
-  System.out.print("Echo"); 
-  System.out.println(); 
-  System.out.print("Foxtrot"); 
-  System.out.println(); 
-  System.out.println("Golf");  
-  System.out.print("Hotel"); 
-  System.out.println(); 
-  System.out.println("India"); 
-  System.out.println(); 
-  System.out.println("This" + " " + "is" + " " + "test."); 
-  System.out.println(); 
-  System.out.print("I'm" + " " + "learning" + " " + "Java");  
-  System.out.println("the" + " " + "hard" + " " + "way."); 
- }
-}
+ //this is the original provided code for PrintingChoices before any changes were made. 
+public class PrintingChoices { 
+   public static void main( String[] args ) { 
+   System.out.println( "Alpha" ); 
+   System.out.println( "Bravo" ); 
+   System.out.println( "Charlie" ); 
+   System.out.println( "Delta" ); 
+   System.out.println(); 
+    
+   System.out.print( "Echo" ); 
+   System.out.print( "Foxtrot" ); 
+   
+   System.out.println( "Golf" ); 
+   System.out.print( "Hotel" ); 
+   System.out.println(); 
+   System.out.println( "India" ); 
+    
+   System.out.println();
+   System.out.println( "This" + " " + "is" + " " + "a" + " test." );  } }
