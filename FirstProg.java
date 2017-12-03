92033bdb56b1b9e5b5c3a5099a0fff312af6b723
Che Perth <chejesusperth@yahoo.com>
2017-12-02 22:43:18 -0800

    Create FirstProg.java
    This is the original code that was already provided in the book that i changed per the lesson requirement. 
    
    
    public class FirstProg
    {
     public static void main(String[]args)
     {
      System.out.println("I am determined to learn how to code." ); 
      System.out.println("Today's date is"); 
     }
    }

---
+++ FirstProg.java
@@ -0,0 +1,17 @@
+
+public class FirstProg
+{
+ public static void main(String[]args)
+ {
+  System.out.println("I, Che Perth, am determined to learn how to code."); 
+  System.out.println("June 1st 2017"); 
+ }
+}
+
+
+
+
+
+
+
+ 
