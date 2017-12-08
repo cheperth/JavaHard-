07a19f00123620bf3e15dbd27d9d44cd2286e88c
Che Perth <chejesusperth@yahoo.com>
2017-12-07 19:27:31 -0800

    Update VariableChangeShortcuts .java

--- VariableChangeShortcuts .java
+++ VariableChangeShortcuts .java
@@ -1,11 +1,12 @@
-/*Learn Java the Hard Way coding assignment. Code provided by author before changes are made.*/ 
+/*Learn Java the Hard Way Assignment after changes are made to assignment.*/ 
 
 
-public class VariableChangeShortcuts 
-{ 
- public static void main( String[] args ) 
- { 
+public class VariableChangeShortcuts
+{
+ public static void main( String[]args )
+ {
   int i, j, k; 
+  
   i = 5; 
   j = 5; 
   k = 5; 
@@ -14,6 +15,7 @@ public class VariableChangeShortcuts
   j = j - 3; 
   k = k * 3; 
   System.out.println( "i: " + i + "\tj: " + j + "\tk: " + k + "\n" ); 
+  
   i = 5; 
   j = 5; 
   k = 5; 
@@ -22,22 +24,29 @@ public class VariableChangeShortcuts
   j -= 3; 
   k *= 3; 
   System.out.println( "i: " + i + "\tj: " + j + "\tk: " + k + "\n" ); 
-  i = j = k = 5; 
-  System.out.println( "i: " + i + "\tj: " + j + "\tk: " + k ); 
-  i += 1; 
-  j -= 2; 
-  k *= 3; 
-  System.out.println( "i: " + i + "\tj: " + j + "\tk: " + k + "\n" ); 
+  
   i = j = 5; 
   System.out.println( "i: " + i + "\tj: " + j ); 
-  i =+ 1; // Oops! 
-  j =- 2; 
-  System.out.println( "i: " + i + "\tj: " + j + "\n" ); 
+  i =+ 1; //Oops!
+  j=- 2; 
+  System.out.println( "i: " + i + "\tj: " + j + "\n"); 
+  
   i = j = 5; 
   System.out.println( "i: " + i + "\tj: " + j ); 
   i++; 
-  j--; 
+  j--;
   System.out.println( "i: " + i + "\tj: " + j ); 
-  }
+  
+  i = 5; 
+  i =-0; 
+  System.out.println( "i=" + i ); 
+  
+  i = 5; 
+  i++; //6
+  i++; //7
+  i++; //8
+  i++; //9
+  i++; //10
+  System.out.println("i =" + i); 
  }
-
+}
