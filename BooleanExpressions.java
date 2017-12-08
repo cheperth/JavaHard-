44d11f278585f12d6a4c5cd93327174ffc8e99ec
Che Perth <chejesusperth@yahoo.com>
2017-12-07 20:07:56 -0800

    Create BooleanExpressions.java

---
+++ BooleanExpressions.java
@@ -0,0 +1,37 @@
+import java.util.Scanner; 
+
+public class BooleanExpressions 
+{
+ public static void main( String[] args ) 
+ { 
+ Scanner keyboard = new Scanner(System.in); 
+ boolean a, b, c, d, e, f; 
+ double x, y; 
+
+ System.out.print( "Give me two numbers. First: " ); 
+ x = keyboard.nextDouble(); 
+ System.out.print( "Second: " ); 
+ y = keyboard.nextDouble();  
+ a = (x < y); 
+ b = (x <= y); 
+ c = (x == y); 
+ d = (x != y); 
+ e = (x > y); 
+ f = (x >= y); 
+ 
+ System.out.println( x + " is LESS THAN " + y + ": " + a );
+ System.out.println( x + " is LESS THAN / EQUAL TO " + y + ": " + b ); 
+ System.out.println( x + " is EQUAL TO " + y + ": " + c ); 
+ System.out.println( x + " is NOT EQUAL TO " + y + ": " + d ); 
+ System.out.println( x + " is GREATER THAN " + y + ": " + e );
+ System.out.println( x + " is GREATER THAN / EQUAL TO " + y + ": " + f ); 
+ System.out.println(); 
+ 
+ System.out.println( !(x < y) + " " + (x >= y) ); 
+ System.out.println( !(x <= y) + " " + (x > y) ); 
+ System.out.println( !(x == y) + " " + (x != y) ); 
+ System.out.println( !(x != y) + " " + (x == y) ); 
+ System.out.println( !(x > y) + " " + (x <= y) ); 
+ System.out.println( !(x >= y) + " " + (x < y) ); 
+  } 
+}  
