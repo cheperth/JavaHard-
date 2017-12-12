0dd0feddf1a6f93a5f69422e7fe2d7b4cb510e09
Che Perth <chejesusperth@yahoo.com>
2017-12-12 15:24:09 -0800

    Update GasolineReceipt.java
     /*
     Learn Java the Hard Way. Exercise 2: More Printing
     This is the original source code provided by the 
     author before any changes were made as well as the 
     Study Drill: 
     This receipt uses gasoline prices for Texas. 
     Go back to your text editor and change thedate 
     and other details to better fit the area where
     you live. Then save it, compile it and run it again.
     */

--- GasolineReceipt.java
+++ GasolineReceipt.java
@@ -1,18 +1,28 @@
-public class GasolineReceipt
-{
- public static void main( String[]args)
- {
-  System.out.println("+--------------------------+");
-  System.out.println("|                          |"); 
-  System.out.println("|       CORNER STORE       |"); 
-  System.out.println("|                          |"); 
-  System.out.println("|  2017-07-11     06:45PM  |"); 
-  System.out.println("|                          |"); 
-  System.out.println("|  Gallons:         20.870 |"); 
-  System.out.println("|  Price/gallon:   $ 4.089 |"); 
-  System.out.println("|                          |"); 
-  System.out.println("|  Fuel total:    $ 85.33  |"); 
-  System.out.println("|                          |"); 
-  System.out.println("+--------------------------+"); 
- }
-}
+ /*
+ Learn Java the Hard Way. Exercise 2: More Printing
+ This is the original source code provided by the 
+ author before any changes were made as well as the 
+ Study Drill: 
+ This receipt uses gasoline prices for Texas. 
+ Go back to your text editor and change thedate 
+ and other details to better fit the area where
+ you live. Then save it, compile it and run it again.
+ */ 
+public class GasolineReceipt 
+   { 
+    public static void main( String[] args ) 
+    {  
+     System.out.println( "+------------------------+" ); 
+     System.out.println( "|                        |" );
+     System.out.println( "|      CORNER STORE      |" ); 
+     System.out.println( "|                        |" ); 
+     System.out.println( "|   2015-03-29 04:38PM   |" ); 
+     System.out.println( "|                        |" ); 
+     System.out.println( "|    Gallons: 10.870     |" ); 
+     System.out.println( "| Price/gallon: $ 2.089  |" ); 
+     System.out.println( "|                        |" ); 
+     System.out.println( "|   Fuel total: $ 22.71  |" ); 
+     System.out.println( "|                        |" ); 
+     System.out.println( "+------------------------+" ); 
+    } 
+   }
