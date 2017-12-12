3244752d446bd4edc3da50cddc15d5f4f77c764b
Che Perth <chejesusperth@yahoo.com>
2017-12-12 14:26:07 -0800

    Update CreatingVariables.java
      /* 
      Learn Java the Hard Way Exercise 5.
      Provided code before any changes are made. 
      Study Drill: Add four more variables to the program: another integer, 
      another double, and two Strings. 
      Name them whatever you want. Give them values. Print them out.
     */

--- CreatingVariables.java
+++ CreatingVariables.java
@@ -1,34 +1,36 @@
-public class CreatingVariables
-{
- public static void main(String[]args)
- {
-  int x, y, age, before; 
-  double seconds, e, checking, beauty; 
-  String firstName, lastName, title, Chest, wall; 
-  
-  x = 10; 
-  y = 400; 
-  age = 39; 
-  before = 69; 
-  
-  beauty = 200.12;
-  seconds = 4.71; 
-  e = 2.71828182845904523536;
-  checking = 1.89; 
-  
-  wall = "nuts"; 
-  Chest = "Nuts"; 
-  firstName = "Graham"; 
-  lastName = "Mitchell";
-  title = "Mr."; 
-  
-  System.out.println("The variable x contains " + x); 
-  System.out.println("The value " + y + " is stored in the variable y." ); 
-  System.out.println("The experiment took " + seconds + " seconds." ); 
-  System.out.println("A favorite irrational # is Euler's number:" + e ); 
-  System.out.println("Hopefully you have more than $" + checking + "!" ); 
-  System.out.println("My name's " + title + " " + firstName + lastName ); 
-  System.out.println(age + before + beauty + " " + wall + " " + Chest ); 
- }
-}
-  
+  /* 
+  Learn Java the Hard Way Exercise 5.
+  Provided code before any changes are made. 
+  Study Drill: Add four more variables to the program: another integer, 
+  another double, and two Strings. 
+  Name them whatever you want. Give them values. Print them out.
+ */ 
+
+
+
+
+public class CreatingVariables 
+  { 
+   public static void main( String[] args ) 
+   {  
+    int x, y, age; 
+    double seconds, e, checking; 
+    String firstName, lastName, title; 
+       
+    x = 10; 
+    y = 400; 
+    age = 39;
+    seconds = 4.71; 
+    e = 2.71828182845904523536; 
+    checking = 1.89; 
+    firstName = "Graham"; 
+    lastName = "Mitchell"; 
+    title = "Mr."; 
+    System.out.println( "The variable x contains " + x ); 
+    System.out.println( "The value " + y + " is stored in the variable y." ); 
+    System.out.println( "The experiment took " + seconds + " seconds." ); 
+    System.out.println( "A favorite irrational # is Euler's number: " + e ); 
+    System.out.println( "Hopefully you have more than $" + checking + "!" ); 
+    System.out.println( "My name's " + title + " " + firstName + lastName ); 
+    } 
+   }
