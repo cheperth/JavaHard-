2c4b236e75cb07a3f6acda5a2b8f290925dd4033
Che Perth <chejesusperth@yahoo.com>
2017-12-12 14:33:09 -0800

    Update FirstProg.java
     /*
      Learn Java the Hard Way Exercise 1.
      Provided code after changes were made. 
      1. Change what is inside the quotes on line 4 to include today’s date. Save the file once
      you have made your changes, compile the file and run it again. 
      2. Change what is inside the quotes on line 3 to have the computer display your name.
    */

--- FirstProg.java
+++ FirstProg.java
@@ -1,8 +1,9 @@
  /*
-	Provided code, study drill provided code after changes were made 
- 1. Change what is inside the quotes on line 4 to include today’s date.
-	Save the file once you have made your changes, compile the file and run it again. 
-2. Change what is inside the quotes on line 3 to have the computer display your name.
+  Learn Java the Hard Way Exercise 1.
+  Provided code after changes were made. 
+  1. Change what is inside the quotes on line 4 to include today’s date. Save the file once
+  you have made your changes, compile the file and run it again. 
+  2. Change what is inside the quotes on line 3 to have the computer display your name.
 */ 
 public class FirstProg
 {
