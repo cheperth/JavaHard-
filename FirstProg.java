cd7f78418bec7471f6d02c71d58bd76439102596
Che Perth <chejesusperth@yahoo.com>
2017-12-12 14:12:06 -0800

    Update FirstProg.java
    Provided code and study drill before any changes are made 
     1. Change what is inside the quotes on line 4 to include today’s date. Save the file once you have made your changes, compile the file and run it again. 
    2. Change what is inside the quotes on line 3 to have the computer display your name.

--- FirstProg.java
+++ FirstProg.java
@@ -1,14 +1,17 @@
-
-public class FirstProg
-{
- public static void main(String[]args)
- {
-  System.out.println("I, Che Perth, am determined to learn how to code."); 
-  System.out.println("June 1st 2017"); 
+ /*
+	Provided code and study drill before any changes are made 
+ 1. Change what is inside the quotes on line 4 to include today’s date.
+	Save the file once you have made your changes, compile the file and run it again. 
+2. Change what is inside the quotes on line 3 to have the computer display your name.
+*/ 
+public class FirstProg 
+ { 
+  public static void main( String[] args ) 
+  {  
+   System.out.println( "I am determined to learn how to code." ); 
+   System.out.println( "Today's date is" ); 
+  } 
  }
-}
-
-
 
 
 
