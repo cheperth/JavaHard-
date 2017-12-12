26b67a180e8c8b7542b26203eac16fd221be52af
Che Perth <chejesusperth@yahoo.com>
2017-12-12 15:38:29 -0800

    Update PrintingChoices.java
    /*
    Learn Java the Hard Way. 
    Exercise 3 : PrintingChoices
    This is the original source code provided by the author 
    before any changes are made to it. 
    Here is the Study Drill for the lesson: 
    1. Add a printing statement at the end of the code to display the sentence 
    “I am learning Java the Hard Way!” But break it up like line 19 so that you 
    only have two or three words in each String and plus signs between. Make 
    sure the spaces get included!
    */

--- PrintingChoices.java
+++ PrintingChoices.java
@@ -1,23 +1,35 @@
-public class PrintingChoices
-{
- public static void main( String[]args)
- {
-  System.out.println("Alpha");
-  System.out.println("Bravo"); 
-  System.out.println("Charlie"); 
-  System.out.println("Delta"); 
-  System.out.print("Echo"); 
-  System.out.println(); 
-  System.out.print("Foxtrot"); 
-  System.out.println(); 
-  System.out.println("Golf");  
-  System.out.print("Hotel"); 
-  System.out.println(); 
-  System.out.println("India"); 
-  System.out.println(); 
-  System.out.println("This" + " " + "is" + " " + "test."); 
-  System.out.println(); 
-  System.out.print("I'm" + " " + "learning" + " " + "Java");  
-  System.out.println("the" + " " + "hard" + " " + "way."); 
- }
+/*
+Learn Java the Hard Way. 
+Exercise 3 : PrintingChoices
+This is the original source code provided by the author 
+before any changes are made to it. 
+Here is the Study Drill for the lesson: 
+1. Add a printing statement at the end of the code to display the sentence 
+“I am learning Java the Hard Way!” But break it up like line 19 so that you 
+only have two or three words in each String and plus signs between. Make 
+sure the spaces get included!
+*/ 
+
+
+public class PrintingChoices {
+	public static void main( String[] args ) {
+		System.out.println( "Alpha" );
+		System.out.println( "Bravo" );
+
+		System.out.println( "Charlie" );
+		System.out.println( "Delta" );
+		System.out.println();
+
+		System.out.print( "Echo" );
+		System.out.print( "Foxtrot" );
+
+		System.out.println( "Golf" );
+		System.out.print( "Hotel" );
+		System.out.println();
+		System.out.println( "India" );
+
+		System.out.println();
+		System.out.println( "This" + " " + "is" + " " + "a" + " test." );
+	}
 }
+     
