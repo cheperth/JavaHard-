552aa66ce7a19d03ca4d813499af5ef7906cae1a
Che Perth <chejesusperth@yahoo.com>
2017-12-12 15:46:52 -0800

    Update PrintingChoices.java
    /*
    Learn Java the Hard Way. 
    Exercise 3 : PrintingChoices
    This is the original source code provided by the author 
    before any changes are made to it. 
    Here is the Study Drill for the lesson: 
    1. Add a printing statement at the end of the code to display the sentence 
    “I am learning Java the Hard Way!” But break it up like line 19 so that you 
    only have two or three words in each String and plus signs between. Make 
    sure the spaces get included!
    */

--- PrintingChoices.java
+++ PrintingChoices.java
@@ -11,25 +11,27 @@ sure the spaces get included!
 */ 
 
 
-public class PrintingChoices {
-	public static void main( String[] args ) {
-		System.out.println( "Alpha" );
-		System.out.println( "Bravo" );
+public class PrintingChoices 
+{
+ public static void main( String[] args ) 
+ {
+  System.out.println( "Alpha" );
+  System.out.println( "Bravo" );
 
-		System.out.println( "Charlie" );
-		System.out.println( "Delta" );
-		System.out.println();
+  System.out.println( "Charlie" );
+  System.out.println( "Delta" );
+  System.out.println();
 
-		System.out.print( "Echo" );
-		System.out.print( "Foxtrot" );
+  System.out.print( "Echo" );
+  System.out.print( "Foxtrot" );
 
-		System.out.println( "Golf" );
-		System.out.print( "Hotel" );
-		System.out.println();
-		System.out.println( "India" );
+  System.out.println( "Golf" );
+  System.out.print( "Hotel" );
+  System.out.println();
+  System.out.println( "India" );
 
-		System.out.println();
-		System.out.println( "This" + " " + "is" + " " + "a" + " test." );
-	}
+  System.out.println();
+  System.out.println( "This" + " " + "is" + " " + "a" + " test." );
+ }
 }
      
