4f7ac933e44449683ad8ca8b038a0bf6bc8620c1
Che Perth <chejesusperth@yahoo.com>
2017-12-12 16:18:49 -0800

    Update RudeQuestions.java
    /*
    Learn Java the Hard Way. Lesson 8: Storing the humans responses.
    This is the source code provided by the author before any 
    changes are made. 
    There is the Study Drill for the lesson: 
    1) Add a new variable of your choosing. Add another question. (It doesn’t have to be rude.) 
    Let the human put an answer to your question into your new variable, 
    and display the human’s answer on the screen afterward.
    */

--- RudeQuestions.java
+++ RudeQuestions.java
@@ -1,39 +1,34 @@
-/*Learn Java the Hard Way after changes are made.*/ 
+/*
+Learn Java the Hard Way. Lesson 8: Storing the humans responses.
+This is the source code provided by the author before any 
+changes are made. 
+There is the Study Drill for the lesson: 
+1) Add a new variable of your choosing. Add another question. (It doesn’t have to be rude.) 
+Let the human put an answer to your question into your new variable, 
+and display the human’s answer on the screen afterward.
+*/ 
 
-mport java.util.Scanner; 
+public class RudeQuestionsImpliedTypes {
+	public static void main( String[] args ) {
+		keyboard = new Scanner(System.in);
 
-public class RudeQuestions
-{
- public static void main(String[]args)
- {
-  String name; 
-  int age, weight; 
-  double income, bankBalance; 
- 
-  Scanner keyboard = new Scanner(System.in); 
- 
-  System.out.print( "Hello. What is your name?" ); 
-  name = keyboard.next(); 
- 
-  System.out.print( "Hi, " + name + "!" + " " + "How old are you?" ); 
-  age = keyboard.nextInt(); 
- 
-  System.out.println( "so you're " + age + ", eh? That's not very old." ); 
-  System.out.print( "How much do you weigh, " + name + "? " ); 
-  weight = keyboard.nextInt(); 
- 
-  System.out.println( weight + "! Better keep that quite!!" ); 
-  System.out.print( "Finally, what's your income, " + name + "? " ); 
-  income = keyboard.nextDouble(); 
-  
-  System.out.print( "Hopefully that is " + income + " per hour" ); 
-  System.out.println( " and not per year!" ); 
-  
-  System.out.print( "What is your bank balance?" ); 
-  bankBalance = keyboard.nextDouble(); 
-  System.out.println( "Your bank balance is " + bankBalance + ".\n" + "That's fucked up!!!!");
-  System.out.print( "Well, thanks for answering my rude question, " ); 
-  System.out.println( name + "." ); 
- }
+		System.out.print( "Hello. What is your name? " );
+		name = keyboard.next();
+
+		System.out.print( "Hi, " + name + "! How old are you? " );
+		age = keyboard.nextInt();
+
+		System.out.println( "So you're " + age + ", eh? That's not very old." );
+		System.out.print( "How much do you weigh, " + name + "? " );
+		weight = keyboard.nextDouble();
+
+		System.out.println( weight + "! Better keep that quiet!!" );
+		System.out.print("Finally, what's your income, " + name + "? " );
+		income = keyboard.nextDouble();
+
+		System.out.print( "Hopefully that is " + income + " per hour" );
+		System.out.println( " and not per year!" );
+		System.out.print( "Well, thanks for answering my rude questions, " );
+		System.out.println( name + "." );
+	}
 }
- 
