10ccf4eb688a99947a21253d15455b8af7db78cb
Che Perth <chejesusperth@yahoo.com>
2017-12-13 14:29:31 -0800

    Update GenderTitles.java
    Learn Java The Hard Way lesson 21 : Nested If Statements
    
    This is the code after the changes from the study drill are applied.
    
    Study Drill: 
    
    Change the else on line 31 to a suitable if statement instead, like:
    
    if ( gender.equals("M") )

--- GenderTitles.java
+++ GenderTitles.java
@@ -1,8 +1,7 @@
 /*
 Learn Java The Hard Way lesson 21 : Nested If Statements
 
-This is the original source code provided by the book before 
-any of the changes from the study drill are applied.
+This is the code after the changes from the study drill are applied.
 
 Study Drill: 
 
@@ -11,43 +10,45 @@ Change the else on line 31 to a suitable if statement instead, like:
 if ( gender.equals("M") )
 */
 
-import java.util.Scanner;
+import java.util.Scanner; 
 
-public class GenderTitles 
+public class GenderTitles
 {
-	public static void main( String[] args ) 
-   {
-		Scanner keyboard = new Scanner(System.in);
-		String title;
-
-		System.out.print( "First name: " );
-		String first = keyboard.next();
-		System.out.print( "Last name: " );
-		String last = keyboard.next();
-		System.out.print( "Gender (M/F): " );
-		String gender = keyboard.next();
-		System.out.print( "Age: " );
-		int age = keyboard.nextInt();
-
-		if ( age < 20 ) {
-			title = first;
-		}
-		else {
-			if ( gender.equals("F") ) {
-				System.out.print( "Are you married, "+first+"? (Y/N): " );
-				String married = keyboard.next();
-				if ( married.equals("Y") ) {
-					title = "Mrs.";
-				}
-				else {
-					title = "Ms.";
-				}
-			}
-			else {
-				title = "Mr.";
-			}
-		}
-
-		System.out.println( "\n" + title + " " + last );
+ public static void main( String[]args)
+ {
+  Scanner keyboard = new Scanner(System.in); 
+  String title; 
+  
+  System.out.print( "First name: "); 
+  String first = keyboard.next(); 
+  System.out.print( "Last name: "); 
+  String last = keyboard.next(); 
+  System.out.print("Gender (M/F): "); 
+  String gender = keyboard.next(); 
+  System.out.print( "Age: "); 
+  int age = keyboard.nextInt(); 
+  
+  if (age < 20 ) {
+   title = first; 
+  }
+  else {
+   if (gender.equals("F") ){
+    System.out.print( "Are you married, " +first+"?(Y/N):"); 
+	String married = keyboard.next(); 
+	if ( married.equals("Y") ){
+	 title = "Mrs "; 
 	}
+	else {
+	 title = "Ms "; 
+	} 
+   }
+   else{
+    title = "Mr."; 
+   }
+  }
+  System.out.println("\n" + title + " " + last ); 
+ }
 }
+
+
+	 
