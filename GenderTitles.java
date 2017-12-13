1bce09d1b95e809b0d8d5ce2b0b96dac6f028e3b
Che Perth <chejesusperth@yahoo.com>
2017-12-13 14:27:13 -0800

    Create GenderTitles.java
    Learn Java The Hard Way lesson 21 : Nested If Statements
    
    This is the original source code provided by the book before 
    any of the changes from the study drill are applied.
    
    Study Drill: 
    
    Change the else on line 31 to a suitable if statement instead, like:
    
    if ( gender.equals("M") )

---
+++ GenderTitles.java
@@ -0,0 +1,53 @@
+/*
+Learn Java The Hard Way lesson 21 : Nested If Statements
+
+This is the original source code provided by the book before 
+any of the changes from the study drill are applied.
+
+Study Drill: 
+
+Change the else on line 31 to a suitable if statement instead, like:
+
+if ( gender.equals("M") )
+*/
+
+import java.util.Scanner;
+
+public class GenderTitles 
+{
+	public static void main( String[] args ) 
+   {
+		Scanner keyboard = new Scanner(System.in);
+		String title;
+
+		System.out.print( "First name: " );
+		String first = keyboard.next();
+		System.out.print( "Last name: " );
+		String last = keyboard.next();
+		System.out.print( "Gender (M/F): " );
+		String gender = keyboard.next();
+		System.out.print( "Age: " );
+		int age = keyboard.nextInt();
+
+		if ( age < 20 ) {
+			title = first;
+		}
+		else {
+			if ( gender.equals("F") ) {
+				System.out.print( "Are you married, "+first+"? (Y/N): " );
+				String married = keyboard.next();
+				if ( married.equals("Y") ) {
+					title = "Mrs.";
+				}
+				else {
+					title = "Ms.";
+				}
+			}
+			else {
+				title = "Mr.";
+			}
+		}
+
+		System.out.println( "\n" + title + " " + last );
+	}
+}
