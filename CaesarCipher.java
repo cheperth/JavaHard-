938fd3443ead7929bd472ee2db868fc26e4a691f
Che Perth <chejesusperth@yahoo.com>
2017-12-14 20:35:23 -0800

    Create CaesarCipher.java
    Learn Java the Hard Way 45: Caesar Cipher (Looping ThroughaString)
    This is the original code provided by the book before any changes 
    from the study drill were applied. 
    
    Study Drill: 
    
    Modify the code to read the plain text message from a text file 
    instead of letting the human type it in. Then, instead of just 
    displaying the ciphertext on the screen, add code to store the 
    ciphertext into a file.

---
+++ CaesarCipher.java
@@ -0,0 +1,61 @@
+/*
+Learn Java the Hard Way 45: Caesar Cipher (Looping ThroughaString)
+This is the original code provided by the book before any changes 
+from the study drill were applied. 
+
+Study Drill: 
+
+Modify the code to read the plain text message from a text file 
+instead of letting the human type it in. Then, instead of just 
+displaying the ciphertext on the screen, add code to store the 
+ciphertext into a file.
+*/ 
+
+import java.util.Scanner;
+import java.io.File; 
+import java.io.PrintWriter; 
+
+
+public class CaesarCipher {
+	/**
+	 * Returns the character shifted by the given number of letters.
+	 */
+	public static char shiftLetter( char c, int n ) {
+		int u = c;
+
+		if ( ! Character.isLetter(c) )
+			return c;
+
+		u = u + n;
+		if ( Character.isUpperCase(c) && u > 'Z'
+	      || Character.isLowerCase(c) && u > 'z' ) {
+			u -= 26;
+		}
+		if ( Character.isUpperCase(c) && u < 'A'
+		  || Character.isLowerCase(c) && u < 'a' ) {
+			u += 26;
+		}
+
+		return (char)u;
+	}
+
+	public static void main( String[] args ) throws Exception {
+		Scanner keyboard = new Scanner(System.in);
+		String plaintext, cipher = "";
+		int shift; 
+         
+		Scanner fileIn = new Scanner(new File("cipher-in.txt")); 
+		plaintext = fileIn.nextLine();
+		shift = fileIn.nextInt(); 
+		fileIn.close(); 
+		
+		System.out.println("Encrypting message: " + plaintext); 
+		System.out.println("Using shift: " + shift); 
+		
+
+		for ( int i=0; i<plaintext.length(); i++ ) {
+			cipher += shiftLetter( plaintext.charAt(i));
+		}
+		System.out.println( cipher );
+	}
+}
